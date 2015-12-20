7927e93112c89307f5638b38be80c3189b020380
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2015-12-21 02:30:22 +0530

    348 solved

--- 348/348.cpp
+++ 348/348.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 using namespace std;
 
+//  returns true if it is a palindrome
 inline bool palindrome(long long num) {
     const string str = to_string(num);
     const int len = str.length();
@@ -12,36 +13,31 @@ inline bool palindrome(long long num) {
 
 //  assuming all the numbers are less than n defined below
 const int n = (int)1e9;
-int counter[n];
+unsigned char counter[n];
 
 int main() {
     //  this loop should take a while
     //  itll take a few seconds
-    for(int i = 2; i < 1000; ++i) {
-        long long x = i * i * i;
-        for(int j = 2; j < 100000; ++j) {
-            long long y = x + (j*j);
-            if(y < n) counter[y]++;
-            else break;
-        }
-    }
+    int x, y;
+    for(int i = 2; (x = i * i * i) < n; ++i)
+        for(int j = 2; (y = x + j * j) < n; ++j)
+            counter[y]++;
+    
 
     long long sum = 0;
     int countdown = 5;
-    for(int i = 0; i < n && countdown; ++i) if (counter[i] == 4) {
-        if(palindrome(i)) {
-            sum += i;
-            countdown--;
-        } 
-    }
+    
+    for(int i = 0; i < n && countdown; ++i) 
+        if (counter[i] == 4 && palindrome(i))
+            sum += i, countdown--;
+
     printf("%lld\n", sum);
 }
 /*
- *
  * 5229225
  * 37088073
  * 56200265
  * 108909801
  * 796767697
  * sum = 1004195061
- * /
+ */
