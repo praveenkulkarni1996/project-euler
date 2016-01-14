94f1b74a85ce0dbf9d2b32bf7d57659b148bface
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2016-01-14 16:07:42 +0530

    345 cpp fast solution

--- 345/345.cpp
+++ 345/345.cpp
@@ -1,43 +1,28 @@
 #include <iostream>
-#include <algorithm>
-#include <cstdio>
-#include <ctime>
 using namespace std;
 
 const int n = 15;
-int p[n];
-int num[n][n];
+const int N = 1 << n;
+short int a[n][n];
+short int dp[N];
 
-// brute force, should take a day to solve
-// should be able to improve this by a factor of n by implementing permutation mechanics
-// multithreading would be nice.
-// started running at about 6:35 pm on 6 january 2015
-// stopped it at ~ 1:30 the following day, 7 january it gave the corrct answer = 13938 
-// but hadnt finished running, ill do this with dp and/or the hungarian algorithm 
-void solver() {
-    for(int i = 0; i < n; ++i) 
-        p[i] = i;
-    
-    for(int i = 0; i < n; ++i) {
-        for(int j = 0; j < n; ++j) {
-            scanf("%d", &num[i][j]);
-            printf("%d\t", num[i][j]);
-        }
-        printf("\n");
+inline void get_dp(int x) {
+    int y = 0, p = __builtin_popcount(x) - 1;
+    while((1 << y) <= x) {
+        int z = x & (1 << y) ;
+        if(z and dp[x] < dp[x - z] + a[y][p])
+            dp[x] = dp[x - z] + a[y][p];
+        ++y;
     }
-    int msum = 0;
-    do {
-        int sum = 0;
-        for(int i = 0; i < n; ++i) {
-            sum += num[i][p[i]];
-        }
-        if(msum < sum) msum = sum, printf("%d\n", msum); 
-    } while(next_permutation(p, p + n));
-    printf("answer = %d\n", msum);
+}
+inline void solver() {
+    for(int i = 0; i < n; ++i) { for(int j = 0; j < n; ++j) { scanf("%hd", &a[i][j]); } }
+    for(int i = 1; i < N; ++i) get_dp(i);
+    printf("answer = %d\n", dp[N - 1]); 
 }
 
 int main() {
-    freopen("p345_input.txt", "r", stdin); 
+    freopen("p345_input.txt", "r", stdin);
     clock_t start = clock();
     solver();
     clock_t end = clock();
