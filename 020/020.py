2b84a9214921fbdf09bd55a38e86819911d3f210
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2016-01-14 11:57:59 +0530

    020 using reduce() in python

--- 020/020.py
+++ 020/020.py
@@ -1,3 +1,15 @@
-fact = 1;
-for i in xrange(1, 101) : fact *= i
-print sum(int(z) for z in str(fact))
+""" Print the sum of digits in 100!
+    This is a very easy question , 
+    I choose to learn python functional programming features from this
+    I have used reduce(x, y, z)
+        x is a binary operator that returns a value
+        y is an iterable object (list, generator .... )
+        z is the starting value.
+    For example x = mul
+    y = [10, 20, 30]
+    z = 1
+    reduce(x, y, z) = (((1 * 10) * 20) * 30)  
+    sum is short for reduce(add, y, 0)
+"""
+from operator import mul 
+print sum(int(z) for z in str(reduce (mul , xrange(1, 101), 1)))
